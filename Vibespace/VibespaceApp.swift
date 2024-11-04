83a4c40849baa7000a3240727f4b421a0f82fb51
Kevin Doyle Jr <kdoyle@infinitumimagery.com>
2024-11-04 12:48:07 -0500

    Initial Commit

---
+++ Vibespace/VibespaceApp.swift
@@ -0,0 +1,32 @@
+//
+//  VibespaceApp.swift
+//  Vibespace
+//
+//  Created by Kevin Doyle Jr. on 11/4/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VibespaceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ VibespaceTests/VibespaceTests.swift
@@ -0,0 +1,16 @@
+//
+//  VibespaceTests.swift
+//  VibespaceTests
+//
+//  Created by Kevin Doyle Jr. on 11/4/24.
+//
+
+import Testing
+
+struct VibespaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
